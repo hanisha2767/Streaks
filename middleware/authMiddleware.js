039ae28cd04b054fb3195f9575e686a4b60e0326
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2025-11-19 11:35:28 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,14 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
+module.exports = function(req, res, next) {
     const token = req.header("Authorization");
-
     if (!token) {
-        return res.status(401).json({ msg: "No token, access denied" });
+        return res.status(401).json({ msg: "No token, auth denied" });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
+        req.user = decoded.user;      // IMPORTANT
         next();
     } catch (err) {
         res.status(401).json({ msg: "Invalid token" });
