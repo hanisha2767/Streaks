e4669a8d0826a4ef4179c72666b67dc60a22c3b7
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2025-11-26 18:14:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is alive");
+});
+
 app.use('/auth', authRoutes);
 app.use("/tasks", taskRoutes);
 
@@ -21,4 +25,3 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
 });
-
