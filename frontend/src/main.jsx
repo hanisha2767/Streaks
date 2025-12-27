d0fff476ea392a267a48ddf83ef7de32ab49edce
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2025-12-27 17:33:47 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles-main.css"; // 👈 ADD THIS
+import "./css/styles-main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
