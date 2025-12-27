8ed4f6407c58a1ba4d3911d7f15345a9e8e39e1c
hanisha2767 <hanishareddy.cs24@bmsce.ac.in>
2025-12-27 17:36:01 +0530

    Update supabaseClient.js

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+const { createClient } = require("@supabase/supabase-js");
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+module.exports = supabase;
